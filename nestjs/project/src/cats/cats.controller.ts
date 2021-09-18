9d7f0be372a59f138810ff99ca95cb6a4adf35c8
jinsuSang <climbplant39@kakao.com>
2021-09-18 23:23:11 +0900

    pipe and exception

--- nestjs/project/src/cats/cats.controller.ts
+++ nestjs/project/src/cats/cats.controller.ts
@@ -3,24 +3,29 @@ import {
   Delete,
   Get,
   HttpException,
-  HttpStatus,
+  Param,
+  ParseIntPipe,
   Patch,
   Post,
   Put,
+  UseFilters,
 } from '@nestjs/common'
 import { CatsService } from './cats.service'
+import { HttpExceptionFilter } from '../http-exception.filter'
+import { PositiveIntPipe } from '../positive-int.pipe'
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getAllCats() {
     throw new HttpException('api is broken', 401)
   }
 
   @Get(':id')
-  getCat() {
+  getCat(@Param('id', ParseIntPipe, PositiveIntPipe) id: number) {
     return 'cat'
   }
 
